ba454cc648c0f2027c19b0f411bbb534335df810
egin10 <egin066@gmail.com>
2023-04-16 02:04:38 +0700

    Create Random Function for 25 person

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,45 +1,119 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { dataContestants, dataWinners, maxWinner } from "../data";
-import { Utils } from "../Utils";
 
 function Home() {
-  // const [winner, setWinner] = useState("...");
   const [winners, setWinners] = useState([]);
+  const [start, setStart] = useState(false);
 
-  async function pickAWinner() {
-    if (dataWinners.length === maxWinner) {
-      return setWinners(
-        "Ow ow, you have reached the maximum number of the winners"
-      );
+  function pickAWinner() {
+    if (start) return;
+
+    setStart(true);
+
+    let count = 0;
+    const maxRandomTimes = 30;
+    const timer = setInterval(generateTheWinners, 100);
+
+    function generateTheWinners() {
+      // SetInterval Done
+      if (count === maxRandomTimes) {
+        setStart(false);
+
+        return clearInterval(timer);
+      } else {
+        // reset
+        setWinners([]);
+
+        if (dataWinners.length === maxWinner) {
+          return setWinners([
+            "Ow ow, you have reached the maximum number of the winners",
+          ]);
+        }
+
+        scrambling();
+
+        count++;
+      }
+    }
+  }
+
+  function scrambling() {
+    const maxNumberOfWinner = 25;
+    let timers = [];
+
+    for (let win = 0; win < maxNumberOfWinner; win++) {
+      // Random Index of Winners from dataContestants
+      timers[win] = Math.floor(Math.random() * dataContestants.length);
+      const theWinner = dataContestants[timers[win]];
+
+      // Show the winners
+      setWinners((prev) => [...prev, theWinner]);
+    }
+  }
+
+  useEffect(() => {
+    function pushDataWinners() {
+      winners.forEach((data) => {
+        // Add to dataWinners
+        dataWinners.push(data);
+
+        // Remove from dataContestants
+        const index = dataContestants.findIndex(
+          (contestant) => contestant === data
+        );
+        dataContestants.splice(index, 1);
+      });
     }
 
-    Utils.randomWinner(dataContestants, dataWinners, 25, setWinners);
-  }
+    if (winners.length > 1 && !start && dataWinners.length < maxWinner) {
+      pushDataWinners();
+      console.log(winners);
+    }
+  }, [winners, start]);
 
   return (
     <div className="h-full w-full flex flex-col justify-center items-center space-y-4">
-      <h2 className="font-bold text-white text-6xl">The winner is 😱</h2>
+      <h2 className="font-bold text-white text-6xl">The winner is 🚀</h2>
 
-      <h3
-        className={`font-bold text-white ${
-          dataWinners.length === maxWinner ? "w-3/5 text-center" : ""
-        } ${
-          winners.length > 1 && dataWinners.length === maxWinner
-            ? "text-5xl"
-            : "text-7xl animate-pulse"
-        }`}
-      >
-        {winners}
-      </h3>
+      {winners.length > 1 ? (
+        <div className="grid grid-cols-5 gap-4">
+          {winners.map((winner, index) => (
+            <h3
+              key={index}
+              className="border-2 border-white rounded-xl text-white font-bold p-2 mx-2 text-center"
+            >
+              {winner}
+            </h3>
+          ))}
+        </div>
+      ) : winners.length === 0 ? (
+        ""
+      ) : (
+        <div className="w-full flex justify-center items-center">
+          <h3 className="text-white font-bold p-2 mx-2 text-center text-2xl">
+            {winners[0]}
+          </h3>
+        </div>
+      )}
 
-      <img
-        src="start.png"
-        alt="start.png"
-        width={120}
-        height={120}
-        className="hover:cursor-pointer"
-        onClick={() => pickAWinner()}
-      />
+      {start ? (
+        <img
+          src="loading.png"
+          alt="loading.png"
+          width={50}
+          height={50}
+          className="animate-spin"
+        />
+      ) : (
+        <img
+          src="start.png"
+          alt="start.png"
+          width={120}
+          height={120}
+          className="hover:cursor-pointer"
+          onClick={() => pickAWinner()}
+        />
+      )}
     </div>
   );
 }
--- src/components/Winner.js
+++ src/components/Winner.js
@@ -1,26 +1,28 @@
 import { dataWinners, maxWinner } from "../data";
 
 function Winner() {
-  return dataWinners.length > 0 ? (
-    <div className="w-full p-6">
-      <h2 className="text-white font-bold text-4xl">
-        Winner Winner Chicken Dinner {dataWinners.length}/{maxWinner}:
-      </h2>
-      <div className="grid grid-cols-3 gap-10 w-full overflow-y-auto px-6 my-8">
-        {dataWinners.map((data, index) => (
-          <div
-            key={index}
-            className="border-t-2 border-x-2 border-white px-2 py-3 rounded-full flex justify-center items-center font-semibold text-xl max-h-min text-white"
-          >
-            {data}
-          </div>
-        ))}
+  return (
+    <>
+      {dataWinners.length > 0 ? (
+        <div className="grid grid-cols-5 gap-10 w-full h-full overflow-y-auto px-6 mt-8">
+          {dataWinners.map((data, index) => (
+            <div
+              key={index}
+              className="border-b-2 border-x-2 border-white px-2 py-3 rounded-full flex justify-center items-center font-semibold text-xl max-h-min text-white"
+            >
+              {data}
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className="w-full h-full flex justify-center items-center font-semibold text-2xl text-white">
+          Empty
+        </div>
+      )}
+      <div className="sticky bottom-0 w-full text-xl font-semibold text-end px-6 py-2 text-white">
+        Total Winners : {dataWinners.length}/{maxWinner}
       </div>
-    </div>
-  ) : (
-    <div className="w-full h-full flex justify-center items-center font-semibold text-2xl text-white">
-      Empty
-    </div>
+    </>
   );
 }
 
